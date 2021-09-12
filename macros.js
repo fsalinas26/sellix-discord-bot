ec4ee6734a56fd7d06042e4a0000ebec7b683214
fsalinas26 <salinasf3@gmail.com>
2021-09-11 22:18:05 -0700

    display macros

---
+++ macros.js
@@ -0,0 +1,21 @@
+let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+function displayDate(time)
+{
+    let day = new Date(time*1000);
+    var str_out = "";
+    str_out+= `${months[day.getMonth()]} ${day.getDate()} at ${day.getHours()%12}:${day.getMinutes()} ${day.getHours() < 12 ? "AM":"PM"}`
+    return str_out;
+}
+
+function displayTime(time)
+{
+    let day = new Date(time*1000);
+    var str_out = "";
+    str_out+= `${day.getHours()%12}:${day.getMinutes()} ${day.getHours() < 12 ? "AM":"PM"}`
+    return str_out;
+}
+
+module.exports = {
+    displayDate: displayDate,
+    displayTime: displayTime
+}

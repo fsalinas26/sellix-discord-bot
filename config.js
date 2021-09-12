af0634ce99434813083c887c6f7a4501fdaadaee
fsalinas26 <salinasf3@gmail.com>
2021-09-11 17:32:24 -0700

    CLI script for config.json

---
+++ config.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer')
+const fs = require('fs');
+
+var questions = [
+  {
+    type: 'input',
+    name: 'token',
+    message: "What's your Discord Bot Token?"
+  },
+  {
+    type: 'input',
+    name: 'sellix_auth',
+    message: "What's your Sellix API Key?"
+  },
+  {
+    type: 'input',
+    name: 'prefix',
+    message: "What command prefix do you want to use?"
+  },
+  {
+    type: 'input',
+    name: 'admins',
+    message: "Comma-Seperated List of Admin Discord ID's: "
+  }
+]
+
+inquirer.prompt(questions).then(answers => {
+  answers['admins'] = answers['admins'].split(',');
+  fs.writeFile('./config.json',JSON.stringify(answers),function(err,data){
+      console.log(data);
+  });
+  console.log('config.json created successfully');
+})

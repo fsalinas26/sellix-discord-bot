6d10557fa93844613a6e6221c78d0299eb01f282
Francisco Salinas <salinasf3@gmail.com>
2021-09-13 14:03:00 -0700

    Cleanup

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 const eventEmitter = require('events');
 const embeds = require('./commands/embeds');
-const webhook = require('./webhook/webhook')
+const webhook = require('./webhook/webhook');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 client.commands = new Discord.Collection();
 

bbc746d4fc5fb420affd1b38df47d58310d4079a
Francisco Salinas <salinasf3@gmail.com>
2021-09-14 23:06:48 -0700

    Webhook Embeds

---
+++ embeds/Webhooks/WH.feedback_received.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const config = require('../../config.json');
+module.exports = {
+    name: 'feedback:received',
+    Webhook: true,
+    channel: config.webhook_channels[this.name],
+    execute(data){
+        const embed = new Discord.MessageEmbed()
+        .setTitle('New Feedback Recevied')
+        .setURL(`https://dashboard.sellix.io/feedback/reply/${data.invoice.uniqid}?shop=76766`)
+        .setDescription('You have received new feedback!')
+        .setThumbnail('https://cdn.sellix.io/static/logo/single-less-border.png')
+        .addFields(
+            {name: 'Product', value: data.product_title, inline:true},
+            {name: 'Review', value: data.message, inline:true},
+            {name: 'Starts', value: `${data.score}/5`,inline:true},
+        )
+        .setFooter('This is a webhook notification from Sellix.io Discord Bot (github/fsalinas26)')
+        .setTimestamp();
+        return embed;        
+    }
+}

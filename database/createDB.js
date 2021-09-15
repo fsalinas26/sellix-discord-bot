0512ec90999bd07bd85c81050cb134c7b9c06777
Francisco Salinas <salinasf3@gmail.com>
2021-09-15 12:56:19 -0700

    Fixed promise wrapping

--- database/createDB.js
+++ database/createDB.js
@@ -1,28 +1,27 @@
 const sqlite3 = require("sqlite3");
 const fs = require('fs');
 function create(){
-        fs.existsSync('./database/users.db',async function(exist){
+        fs.exists('./database/users.db',async function(exist){
             if(exist){
                 console.log('Database already exist!');
                 return;
             }
-            return new Promise(resolve=>{
-                let db = new sqlite3.Database("./database/users.db", (err)=>{
-                if(err){
-                resolve(err.message); 
-                }
-                resolve("Succesfully created Database");
-            }); 
-        
-        db.serialize(function() {
-            db.run("CREATE TABLE Users (OrderID UNIQUE, DiscordID TEXT, RedeemedOn TEXT)");
-            db.each("SELECT * FROM Users", function(err, row) {
-                console.log(row);
-            });
+          return new Promise(resolve=>{
+              let db = new sqlite3.Database("./database/users.db", (err)=>{
+              if(err){
+              resolve(err.message); 
+              }
+              console.log("Succesfully created Database"); 
+              db.serialize(function() {
+                  db.run("CREATE TABLE Users (OrderID UNIQUE, DiscordID TEXT, RedeemedOn TEXT)");
+                  db.each("SELECT * FROM Users", function(err, row) {
+                      console.log(row);
+                  });
+              });
+
+              db.close();
         });
-        
-        db.close();
-        });
+          });
     })
 }
 
